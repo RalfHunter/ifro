fb03c7cf73109c6e3e3815470820e1ddd2007ae4
Silvio Huan Matheus Grein Cristo Ribeiro <silviohuan@gmail.com>
2025-07-18 20:34:36 -0400

    header finalizado

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,12 @@ const app = document.querySelector<HTMLDivElement>("#app")!
 
 const menu = document.querySelector<HTMLImageElement>(".menu")!
 
+const fechar = document.querySelector<HTMLImageElement>(".fechar")
+
 const topo_inferior = document.querySelector<HTMLDivElement>(".topo-inferior")
 
 menu?.addEventListener("click", () =>{
     topo_inferior?.classList.toggle("ativo")
+    menu?.classList.toggle("ativo")
+    fechar?.classList.toggle("ativo")
 })
