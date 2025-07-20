8509fd6a97776f6903cb3ff9586e3555ade55a30
Silvio Huan <silviohuan@gmail.com>
2025-07-19 23:11:37 -0300

    Adicionado main

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
-import './style.css'
+
+import './styles/header/header.css'
+import './styles/header/header2.css'
+import './styles/main/main.css'
 
 const app = document.querySelector<HTMLDivElement>("#app")!
 
@@ -7,9 +10,26 @@ const menu = document.querySelector<HTMLImageElement>(".menu")!
 const fechar = document.querySelector<HTMLImageElement>(".fechar")
 
 const topo_inferior = document.querySelector<HTMLDivElement>(".topo-inferior")
+const menu_lateral = document.querySelector<HTMLDivElement>(".menu-lateral")
+
+document.addEventListener('click', function(event) {
+
+    const alvo = event.target as HTMLElement;
+  if (menu.contains(alvo)) return;
+
+  // ignora cliques dentro da área do menu lateral
+  if (menu_lateral?.contains(alvo)) return;
+
+  // se o menu estiver ativo, dispara o "fechamento"
+  if (menu.classList.contains("ativo")) {
+    menu.click();
+  }
+})
 
 menu?.addEventListener("click", () =>{
     topo_inferior?.classList.toggle("ativo")
     menu?.classList.toggle("ativo")
     fechar?.classList.toggle("ativo")
+    // app?.classList.toggle("ativo")
+    menu_lateral?.classList.toggle("ativo")
 })
